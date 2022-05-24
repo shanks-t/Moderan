696e1a1b241fdb3f4ea1cf7a1859137fcfe0bfc5
shanks-t <shanksleslietrey@gmail.com>
2022-05-24 16:31:23 -0500

    added workspaces

--- network.tf
+++ network.tf
@@ -13,9 +13,10 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "=3.14.0"
 
-  cidr           = var.vpc_cidr_block
-  azs            = slice(data.aws_availability_zones.available.names, 0, (var.vpc_subnet_count))
-  public_subnets = [for subnet in range(var.vpc_subnet_count) : cidrsubnet(var.vpc_cidr_block, 8, subnet)]
+  cidr = var.vpc_cidr_block[terraform.workspace]
+
+  azs            = slice(data.aws_availability_zones.available.names, 0, (var.vpc_subnet_count[terraform.workspace]))
+  public_subnets = [for subnet in range(var.vpc_subnet_count[terraform.workspace]) : cidrsubnet(var.vpc_cidr_block[terraform.workspace], 8, subnet)]
 
   enable_nat_gateway      = false
   enable_dns_hostnames    = var.enable_dns_hostnames
@@ -63,7 +64,7 @@ resource "aws_security_group" "nginx-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
+    cidr_blocks = [var.vpc_cidr_block[terraform.workspace]]
   }
 
   # outbound internet access
--- variables.tf
+++ variables.tf
@@ -17,15 +17,13 @@ variable "enable_dns_hostnames" {
 }
 
 variable "vpc_cidr_block" {
-  type        = string
+  type        = map(string)
   description = "Base CIDR Block for VPC"
-  default     = "10.0.0.0/16"
 }
 
 variable "vpc_subnet_count" {
-  type        = number
+  type        = map(number)
   description = "Number of subnets to create in VPC"
-  default     = 2
 }
 
 variable "map_public_ip_on_launch" {
@@ -35,15 +33,13 @@ variable "map_public_ip_on_launch" {
 }
 
 variable "instance_type" {
-  type        = string
+  type        = map(string)
   description = "Type for EC2 Instance"
-  default     = "t2.micro"
 }
 
 variable "instance_count" {
-  type        = number
+  type        = map(number)
   description = "Number of instances to create in VPC"
-  default     = 2
 }
 
 variable "company" {
